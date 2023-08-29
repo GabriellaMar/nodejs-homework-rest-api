3c2d3b3a27dfce3a7a09fcbe5db40f7d84888be6
Gabriella Marusyak <slipuhinag@gmail.com>
2023-08-29 16:44:14 +0300

    Created rest-api repository

--- models/contacts.js
+++ models/contacts.js
@@ -1,12 +1,51 @@
-// const fs = require('fs/promises')
+ const fs = require('fs/promises')
+ const path = require('path');
+ const { nanoid } = require('nanoid');
 
-const listContacts = async () => {}
+ const contactsPath = path.join(__dirname, 'contacts.json');  
 
-const getContactById = async (contactId) => {}
 
-const removeContact = async (contactId) => {}
+ console.log(contactsPath)
 
-const addContact = async (body) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data)
+}
+
+const getContactById = async (contactId) => {
+  const contacts = await  listContacts();
+  const result = contacts.find(contact => contact.id === contactId);
+  return result || null;}
+
+const removeContact = async (contactId) => {
+  const contacts = await  listContacts();
+  const index = contacts.findIndex(contact => contact.id ===contactId); 
+  if(index === -1){ 
+      return null;
+   }
+
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+ 
+  return result;
+}
+
+const addContact = async ({ name, email, phone }) => { 
+  if (!name || !email || !phone) {
+    throw new Error('Missing required fields');
+  }
+
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
 
 const updateContact = async (contactId, body) => {}
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,21 +1,34 @@
 const express = require('express')
-
 const router = express.Router()
+const contacts = require('../../models/contacts');
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const result = await contacts.listContacts()
+  res.json(result)
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contactId = req.params.contactId;
+  const result = await contacts.getContactById(contactId)
+  res.json(result)
+
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const newContact = req.body;
+  try {
+    const result = await contacts.addContact(newContact);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contactId = req.params.contactId;
+  const result = await contacts.removeContact(contactId)
+  res.json(result)
 })
 
 router.put('/:contactId', async (req, res, next) => {
